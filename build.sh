031201771ddc43a408c66331ab611bd4a034e7aa
MathiasDanzeisen <matze@specialo.de>
2022-03-03 21:02:08 +0100

    remove conan pkg mang

--- build.sh
+++ build.sh
@@ -4,8 +4,6 @@
 rm -r build
 mkdir -p build
 cd build
-export CONAN_SYSREQUIRES_MODE=enabled
-conan install -pr:b=default -pr:h=default -s build_type=Debug ..
-cmake .. -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
+cmake .. -G "Unix Makefiles"
 
 cmake --build .
